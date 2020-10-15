0cc48cc8d645197ac69c985b5d214a81ba4510fc
Jagriti <jagriti.agni@gmail.com>
2020-10-15 15:03:52 +0530

    [Jagriti]ADDED: EmployeePayrollService class and print welcome message

---
+++ src/main/java/com/bl/datastructure/employeepayrollservice/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bl.datastructure.employeepayrollservice;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		System.out.println("Welcom to the Employee Payroll Service.");
+	}
+}
